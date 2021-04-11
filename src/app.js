cbfc023b9075b8d984898f3399b255dbd94638b4
jonneykang <zhang-ning-qiang@163.com>
2021-04-12 01:28:49 +0800

    jkk

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
 app.get("/", (req, res) => res.send("Hello World! Node Web App!"));
 process.on('uncaughtException', function (e) {
     console.log(e);
 });
-
 
 app.listen(port, () => console.log(`jknnodeweb1 app listening on port ${port}!`));
