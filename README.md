554e718fe7ff3ae0c32e2c2e58af68babb27b5a4
jonneykang <zhang-ning-qiang@163.com>
2021-04-12 01:20:36 +0800

    初始化热加载node web

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # jknodeweb1
 jknodeweb1
+
+dev: npm run dev 支持 src 目录热加载
+
+test: npm run test
+
+proc: npm start
---
+++ test/test1.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('test1', () => {
+  it('should be', () => {
+      expect('a').to.equal('a');
+  });
+})
